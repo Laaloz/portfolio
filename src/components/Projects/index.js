d708d41aa216a73c07840fa9467d7d9092dba97d
Laalo Ceesay <laaloceesay@gmail.com>
2021-08-07 01:58:19 +0300

    projects update

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,31 +1,40 @@
 import React from "react";
+import ScrollToTop from "../ScrollToTop";
 import VideoJS from "../Video";
-import { ProjectsContainer, ProjectsWrapper, Famazon, Bg, Summon, H1 } from "./ProjectsPageElements";
+import {
+    ProjectsContainer,
+    ProjectsWrapper,
+    Famazon,
+    Bg,
+    Summon,
+    H1,
+} from "./ProjectsPageElements";
 
 function Projects() {
     return (
         <>
             <ProjectsContainer>
+                <ScrollToTop />
                 <ProjectsWrapper>
                     <VideoJS />
                 </ProjectsWrapper>
 
                 <ProjectsWrapper>
-                <Bg>
-                <H1>Fake Amazon</H1>
-                <a href="https://fir-34a4c.firebaseapp.com/">
-                <Famazon />
-                </a>
-                </Bg>
+                    <Bg>
+                        <H1>Fake Amazon</H1>
+                        <a href="https://fir-34a4c.firebaseapp.com/">
+                            <Famazon />
+                        </a>
+                    </Bg>
                 </ProjectsWrapper>
 
                 <ProjectsWrapper>
-                <Bg>
-                <H1>Summon Research</H1>
-                <a href="https://www.summon.fi/">
-                <Summon />
-                </a>
-                </Bg>
+                    <Bg>
+                        <H1>Summon Research</H1>
+                        <a href="https://www.summon.fi/">
+                            <Summon />
+                        </a>
+                    </Bg>
                 </ProjectsWrapper>
             </ProjectsContainer>
         </>
